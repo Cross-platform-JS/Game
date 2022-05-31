c188785c00ecfcf6968fad2f0c8082b58fdace07
Samael85 <1374126063@qq.com>
2022-05-31 21:42:13 +0800

    2.0

--- CrossPlatformGame/app.js
+++ CrossPlatformGame/app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var hbs = require('hbs');
+// var ejs = require('ejs');
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -11,11 +13,19 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+// app.engine('.html',ejs._express);
+// app.set('view engine', 'html');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ CrossPlatformGame/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'port': '3306',
+        'user': 'root',
+        'password': '123'
+    },
+    //数据库连接
+    'database': 'games',
+    'users_table': 'users'//数据表名称，用于筛选和存储用户排名
+}
